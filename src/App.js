45fa40b45ea9cfeaa9c18175b93f532abf490d8c
anthonyxchan <anthonyxchan1229@gmail.com>
2020-09-23 01:01:50 -0700

    added axios requests, dashboard components and styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import DashboardGenre from "./components/DashboardGenre";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DashboardGenre
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        // true will return poster and false will return thumbnail images
+        isFeaturedRow={true}
+      />
+      <DashboardGenre title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <DashboardGenre title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <DashboardGenre
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <DashboardGenre title="Comedies" fetchUrl={requests.fetchComedyMovies} />
+      <DashboardGenre
+        title="Horror Night"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <DashboardGenre
+        title="Romantic Movies"
+        fetchUrl={requests.fetchRomanticMovies}
+      />
+      <DashboardGenre
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+      />
+      <DashboardGenre
+        title="Crime Movies"
+        fetchUrl={requests.fetchCrimeMovies}
+      />
+      <DashboardGenre
+        title="Science Fiction Movies"
+        fetchUrl={requests.fetchSciFiMovies}
+      />
     </div>
   );
 }
---
+++ src/components/DashboardGenre.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import styled, { css } from "styled-components";
+
+const image_base_url = "https://image.tmdb.org/t/p/original/";
+
+// const ListContainer = styled.div`
+//   margin-left: 10px;
+//   margin-right: 10px;
+//   background: black;
+// `;
+
+const AllRow = styled.div`
+  margin-left: 10px;
+`;
+
+const ImageRow = styled.div`
+  display: flex;
+  overflow-x: auto;
+  overflow-y: hidden;
+  /* add padding to show full image transform */
+  padding: 20px;
+
+  /* hide the scroll bar */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const MovieThumbnail = styled.img`
+  width: 100%;
+  object-fit: contain;
+  max-height: 100px;
+  /* apply delay to image transform when image being hovered */
+  transition: transform 450ms;
+  margin-right: 10px;
+
+  &:hover {
+    transform: scale(1.08);
+  }
+
+  /* conditional rendering, if it is featured row, 
+  poster image will be talled and scale transform is larger */
+  ${({ isFeaturedRow }) =>
+    isFeaturedRow &&
+    css`
+      max-height: 250px;
+      padding: 5px;
+      &:hover {
+        transform: scale(1.15);
+      }
+    `}
+`;
+
+function DashboardGenre(props) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(props.fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+    //if [], run once when the row load and dont run again
+    // have to include props.fetchUrl as it is a dependency, so when fetchUrl change it will rerender
+  }, [props.fetchUrl]);
+
+  return (
+    <AllRow>
+      <h2>{props.title}</h2>
+      <ImageRow>
+        {movies.map((movie) => (
+          <MovieThumbnail
+            // Keys help React identify which items have changed and increase performance
+            key={movie.id}
+            src={`${image_base_url}${
+              props.isFeaturedRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            // need to pass in isFeaturedRow so styledComponents could pick up different css
+            isFeaturedRow={props.isFeaturedRow}
+            alt={movie.name}
+          />
+        ))}
+      </ImageRow>
+    </AllRow>
+  );
+}
+
+export default DashboardGenre;
